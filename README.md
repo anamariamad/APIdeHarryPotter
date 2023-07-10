62e7e78cb393635fd22ea7ea4295be02588c4985
anamariamad <pendrivemorado@yahoo.es>
2023-07-10 10:31:23 +0200

    modificación de read me

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 Este es un proyecto para consumir una API de Harry Potter, ningún gatito murió en su creación...
+
+Para acceder a este repositorio (pongo ejemplo con visual studio code) debes tener intalados los siguientes programas: 
+
+- Node
+
+- Tener creada la estructura de React:
+hƩps://es.react.dev/learn/start-a-new-react-project
+
+- Vite:
+hƩps://vitejs.dev/guide/
+
+
+Para abrir el repositorio escribe en la terminal de VSC "git clone" seguido del nombre del repositorio.
+
+Sigue después las instrucciones que te aparecerán en la pantalla:
+"or create a new repository on the command line
+echo "# APIdePOKEMON" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/anamariamad/APIdePOKEMON.git
+git push -u origin main"
+
+Como vite también utiliza npm escribimos en el terminal: "npm create vite@latest"
+
+Nos aparecera una lista de frameworks, elegimos react.
+
+Seguidamente elegiremos una "variant" de las que aparecen, en este caso, JavaScript.
+
+Una vez creada intruduciremos los comandos:
+- " cd seguido del nombre que hayamos elegido
+- "npm install"
+- " npm run dev"
+
+Y nos va a entregar un puerto de acceso donde poder visualizarlo.
+
+Para acceder al proyecto hacemos click en "open folder".
+
+
--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,47 +1,57 @@
 /* eslint-disable react/prop-types */
 //react arrow component exports
-//rafce 
-import {useState} from 'react'
-import CharactersDetails from './CharactersDetails';
-import './Characters.css'
+//rafce
+import { useState } from "react";
+import CharactersDetails from "./CharactersDetails";
+import "./Characters.css";
 const Characters = (props) => {
-//Estado = almacén de datos
-        const { character } = props
-        const [isOpenDetailsPopUP, setIsOpenDetailsPopUP] = useState(false);     
+  //Estado = almacén de datos
+  const { character } = props;
+  const [isOpenDetailsPopUP, setIsOpenDetailsPopUP] = useState(false);
 
-       const openDetailsPopUp = () => {
-               setIsOpenDetailsPopUP(true)
-       }
+  const openDetailsPopUp = () => {
+    setIsOpenDetailsPopUP(true);
+  };
 
-       const closeDetailsPopUp = () => {
-                setIsOpenDetailsPopUP(false)
-       }
+  const closeDetailsPopUp = () => {
+    setIsOpenDetailsPopUP(false);
+  };
 
-return (
-        <> 
-        <div className="contenedor">               
-                <div className='card' key={character.id}>
-                        <img className='imagen' src= {character.image} alt='personaje'/>
-                        <p>Nombre en la pelicula: <strong> {character.name}</strong> </p>
-                        <p>Nombre actor: <strong> {character.actor} </strong> </p>
-                        <p>Genero: <strong> {character.gender} </strong></p>
-                        <p>Especie: <strong> {character.species} </strong> </p>
-                        <br/> 
-                        <button onClick={openDetailsPopUp} className='button-card-style'> Ver mas detalles </button>
-                </div>
-                {(isOpenDetailsPopUP === true) && (
-                <div className='modal-characters'> 
-                        <CharactersDetails 
-                                character={character} 
-                                key={character.id}
-                                onClose={closeDetailsPopUp}
-                        />
-                </div>
-  )}
-              
+  return (
+    <>
+      <div className="contenedor">
+        <div className="card" key={character.id}>
+          <img className="imagen" src={character.image} alt="personaje" />
+          <p>
+            Nombre en la pelicula: <strong> {character.name}</strong>{" "}
+          </p>
+          <p>
+            Nombre artista: <strong> {character.actor} </strong>{" "}
+          </p>
+          <p>
+            Genero: <strong> {character.gender} </strong>
+          </p>
+          <p>
+            Especie: <strong> {character.species} </strong>{" "}
+          </p>
+          <br />
+          <button onClick={openDetailsPopUp} className="button-card-style">
+            {" "}
+            Ver mas detalles{" "}
+          </button>
         </div>
-        </>
-)}
-      
+        {isOpenDetailsPopUP === true && (
+          <div className="modal-characters">
+            <CharactersDetails
+              character={character}
+              key={character.id}
+              onClose={closeDetailsPopUp}
+            />
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default Characters;
